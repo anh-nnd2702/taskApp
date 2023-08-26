00df998c4da813e81dc5f3565b3eb4a44e91edf7
anh-nnd2702 <128394903+anh-nnd2702@users.noreply.github.com>
2023-08-26 18:40:56 +0700

    Update jwtKey.js

--- server/configs/jwtKey.js
+++ server/configs/jwtKey.js
@@ -1,5 +1,5 @@
 const jwtKey = {
-    secretKey : process.env.SECRET_KEY
+    secretKey : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZHVjYW5oIiwicHJvamVjdCI6Im5kYWpvYiIsInRpbWUiOjI3MDIyMDAwfQ.kbaIcXgA_VJJlTmXAMw0MqkxBHJ-wOR61OmOhC_4Bx0"
 }
 
 module.exports = jwtKey;
